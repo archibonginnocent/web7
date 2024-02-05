98ddd4463c4d438373ce670754fa252a84bc6c37
archibonginnocent <archibonginnocent11@gmail.com>
2024-02-05 10:42:10 +0100

    second commit

---
+++ src/app.js
@@ -0,0 +1,8 @@
+ document.querySelector('.signIn').addEventListener('click', loginPge);
+ 
+
+function loginPge(){
+   window.location.assign('hencework.com/theme/hound/full-width-light/index.html')
+   console.log('confirm')
+}
+// loginPge();
